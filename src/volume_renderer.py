9306f3eccaa13e072f8a45921a0ab9c7de2d6304
Nikolai Kochetkov <professor3222@gmail.com>
2024-05-21 00:35:28 +0100

    after pre-commit format

--- src/volume_renderer.py
+++ src/volume_renderer.py
@@ -1,10 +1,12 @@
 import torch.nn as nn
 
+
 class VolumeRenderer(nn.Module):
     """
     Module to render 2D view of our 3D model
     from the particular angle
     """
+
     def __init__():
         pass
 
@@ -15,7 +17,7 @@ class VolumeRenderer(nn.Module):
         Output of the NeRF model is a colors along a ray and a sigma (color density)
         \gamma = t_{i + 1} - t_{i} - distance between adjacent samples
         \alpha = 1 - exp(-\sigma_{i} * \gamma_{i}) (alpha compositing value)
-        C_{i}(r_{i}) = \sum(T_{i}(\alpha_{i}) * c_{i})  sum from t_near to t_far \\ equation (3) 
+        C_{i}(r_{i}) = \sum(T_{i}(\alpha_{i}) * c_{i})  sum from t_near to t_far \\ equation (3)
         T_{i}(\alpha) = exp(-sum(\alpha))  sum from t_near to i
-        """        
+        """
         pass
