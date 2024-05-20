306d7036bc6d11214ce6dc2a0d61bbb3c7e82a65
Nikolai Kochetkov <professor3222@gmail.com>
2024-05-21 00:33:20 +0100

    initial commit

---
+++ src/nerf_model.py
@@ -0,0 +1,51 @@
+import torch.nn as nn
+import torch
+
+class NerfModel(nn.Module):
+    """
+    This class implements MLP architecture of the Nerf model
+    from https://arxiv.org/pdf/2003.08934 page 18
+    """
+    def __init__(self, input_size_ray=3, input_size_direction=3, net_width=256):
+        super().__init__()
+        self.input_size_ray = input_size_ray
+        self.input_size_direction = input_size_direction
+        self.net_width = net_width
+        
+        self.model_first_part = nn.Sequential(nn.Linear(input_size_ray, net_width), nn.ReLU(inplace=True))
+        for _ in range(3):
+            self.model_first_part = nn.Sequential(self.model_first_part, 
+                                       nn.Linear(net_width, net_width),
+                                       nn.ReLU(inplace=True))
+        
+        
+        self.model_second_part = nn.Sequential(nn.Linear(input_size_ray + net_width, net_width),
+                                               nn.ReLU(inplace=True))
+        for _ in range(2):
+            self.model_second_part = nn.Sequential(self.model_second_part,
+                                                   nn.Linear(net_width, net_width),
+                                                   nn.ReLU(inplace=True))
+        
+        self.model_second_part = nn.Sequential(self.model_second_part,
+                                               nn.Linear(net_width, net_width))
+
+        self.model_sigma = nn.Linear(net_width + input_size_direction, net_width // 2 + 1)
+        self.model_color = nn.Sequential(nn.ReLU(), nn.Linear(net_width // 2, 3), nn.Sigmoid())
+
+    def forward(self, ray, directions):
+        
+        out = self.model_first_part(ray)
+        # prepare input for the second part ray + out
+        out = torch.cat([ray, out], axis=-1)
+        out = self.model_second_part(out)
+        # prepare input for sigmas and colors out + directions
+        out = torch.cat([directions, out], axis=-1)
+        out = self.model_sigma(out)
+        # extract sigmas and out for colors
+        sigma, out = torch.split(out, [1, self.net_width // 2], dim=-1)
+        
+        color = self.model_color(out)
+
+        return torch.cat([sigma, color], axis=-1)
+
+        
---
+++ src/volume_renderer.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+
+class VolumeRenderer(nn.Module):
+    """
+    Module to render 2D view of our 3D model
+    from the particular angle
+    """
+    def __init__():
+        pass
+
+    def forward(self, nerf_model, ray):
+        """
+        Forward path of a renderer takes output of NeRF MLP and a ray along which
+        we need to create a view.
+        Output of the NeRF model is a colors along a ray and a sigma (color density)
+        \gamma = t_{i + 1} - t_{i} - distance between adjacent samples
+        \alpha = 1 - exp(-\sigma_{i} * \gamma_{i}) (alpha compositing value)
+        C_{i}(r_{i}) = \sum(T_{i}(\alpha_{i}) * c_{i})  sum from t_near to t_far \\ equation (3) 
+        T_{i}(\alpha) = exp(-sum(\alpha))  sum from t_near to i
+        """        
+        pass
