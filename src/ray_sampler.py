4dedaac1299b90627f420fce33c47e2449d7dde8
Nikolai Kochetkov <professor3222@gmail.com>
2024-06-01 22:42:38 +0100

    adding ray sampler, partially tested

--- src/ray_sampler.py
+++ src/ray_sampler.py
@@ -1,7 +1,53 @@
-class RaySampler:
+import torch
+
+
+class RaySampler(torch.nn.Module):
     """
-    This class will be responsible for sampling points along the ray
+    Implements stratified inverse depth sampling
     """
 
-    def __init__():
-        pass
+    def __init__(self, num_samples):
+        super(RaySampler, self).__init__()
+        self.num_samples = num_samples
+
+        # Ray sample count
+        self.point_intervals = torch.linspace(
+            0.0,
+            1.0,
+            self.num_samples,
+            requires_grad=False,
+        )
+
+    def forward(self, ray_count, near, far):
+        """
+        Inputs:
+            ray_count: int, number of rays in input ray chunk
+            near: float or array of shape [BatchSize]. Nearest distance for a ray.
+            far: float or array of shape [BatchSize]. Farthest distance for a ray.
+        Outputs:
+            point_intervals: (ray_count, self.num_samples) : depths of the sampled points along the ray
+        """
+        if not hasattr(near, "shape") and isinstance(near, float):
+            near, far = near * torch.ones_like(
+                torch.empty(ray_count, 1)
+            ), far * torch.ones_like(torch.empty(ray_count, 1))
+        elif len(near.shape) > 0 and near.shape[0] == ray_count:
+            near, far = near[:, None], far[:, None]
+        # The closer to the camera the more samples
+        point_intervals = 1.0 / (
+            1.0 / near * (1.0 - self.point_intervals) + 1.0 / far * self.point_intervals
+        )
+
+        # Get intervals between samples.
+        mids = 0.5 * (point_intervals[..., 1:] + point_intervals[..., :-1])
+        upper = torch.cat((mids, point_intervals[..., -1:]), dim=-1)
+        lower = torch.cat((point_intervals[..., :1], mids), dim=-1)
+
+        # Stratified samples in those intervals.
+        t_rand = torch.rand(
+            point_intervals.shape,
+            dtype=point_intervals.dtype,
+        )
+        point_intervals = lower + (upper - lower) * t_rand
+
+        return point_intervals
