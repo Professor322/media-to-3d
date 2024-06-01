742ed6f3f680213bca2e2b2fc6b9a3c376bd6df2
Nikolai Kochetkov <professor3222@gmail.com>
2024-06-01 21:21:41 +0100

    adding second iteration on dataset (tested)

--- src/dataset.py
+++ src/dataset.py
@@ -8,43 +8,65 @@ from torch.nn import functional as F
 from torch.utils.data import Dataset
 from torchvision import transforms
 
-sys.path.insert(1, "/home/kolek/Edu/project/video-to-3d/src/pycolmap")
-import pycolmap.pycolmap as pycolmap
+sys.path.insert(1, "./pycolmap/")
+import pycolmap
 
 
-def get_pixel_coords(H, W, focal):
-    i, j = torch.meshgrid(torch.linspace(0, W - 1, W), torch.linspace(0, H - 1, H))
+def get_pix2cam(H, W, focal_x, focal_y):
+    i, j = torch.meshgrid(
+        torch.linspace(0, W - 1, W), torch.linspace(0, H - 1, H), indexing="ij"
+    )
     i = i.t()
     j = j.t()
-    # NDC (-1, 1)
+    # Pixel coordinates -> Film coordinates -> Camera Coordinates. Y-axis pointing downwards.
+    # In homogeneous coordinates Z is also pointing downwards
+    # TODO: rewrite in terms of [i, j, 1] @ np.linalg.inv(cam2pix)
     return torch.stack(
-        [(i - W / 2) / focal, -(j - H / 2) / focal, -torch.ones_like(i)], -1
+        [(i - W / 2) / focal_x, -(j - H / 2) / focal_y, -torch.ones_like(i)], -1
     )
 
 
-def get_rays(pixel_coords, c2w):
+def get_cam2pix(focal_length_x, focal_length_y, principal_point_x, principal_point_y):
+    """
+    focal_lenght_x - focal length on x-axis
+    focal_length_y - focal length on y-axix
+    principal_point_x, principal_point_y - camera pinhole coordinates
+
+    This function returns matrix that helps to transform image in the camera coordinates
+    to pixel coordinates. Inverse of this matrix does the opposite, meaming pixel to camera coordinates
+    """
+    return torch.tensor(
+        [
+            [focal_length_x, 0, principal_point_x],
+            [0, focal_length_y, principal_point_y],
+            [0, 0, 1],
+        ]
+    )
+
+
+def get_rays(ray_directions_cam, c2w):
     """
     Get ray origin and normalized directions in world coordinate for all pixels in one image.
     Inputs:
-        directions: (H, W, 3) precomputed ray directions in camera coordinate
+        camera_coordinates: (H, W, 3) precomputed ray directions in camera coordinate
         c2w: (3, 4) transformation matrix from camera coordinate to world coordinate
     Outputs:
         rays_o: (H*W, 3), the origin of the rays in world coordinate
         rays_d: (H*W, 3), the normalized direction of the rays in world coordinate
     """
-    # Rotate ray directions from camera coordinate to the world coordinate
-    rays_d = pixel_coords @ c2w[:, :3].T  # (H, W, 3)
+    # we only need to rotate to align with world coordinates, no need to tranlate.
+    rays_d = ray_directions_cam @ c2w[:3, :3]  # (H, W, 3)
+
     rays_d = rays_d / torch.norm(rays_d, dim=-1, keepdim=True)
     # The origin of all rays is the camera origin in world coordinate
-    rays_o = c2w[:, 3].expand(rays_d.shape)  # (H, W, 3)
-
+    rays_o = c2w[:3, -1].expand(rays_d.shape)  # (H, W, 3)
     rays_d = rays_d.view(-1, 3)
     rays_o = rays_o.view(-1, 3)
 
     return rays_o, rays_d
 
 
-class NerfDataset(Dataset):
+class NerfDatasetRealImages(Dataset):
     """
     Class implements a dataloader for real world images
     Camera positions are estimated using colmap
@@ -58,7 +80,6 @@ class NerfDataset(Dataset):
         self.image_resolution = [image_width, image_height]
         # assuming one camera is used for all pictures
         self.camera = self.scene_manager.cameras[1]
-        # measured in pixels
 
         # matricies to convert from world coordinates to camera coordinates
         self.world2cams = []
@@ -70,29 +91,29 @@ class NerfDataset(Dataset):
         self.transforms = transforms.ToTensor()
         self.near = 2.0
         self.far = 6.0
-        self.pixel_coords = get_pixel_coords(
-            self.image_resolution[0], self.image_resolution[1], self.camera.fx
+        # ray directions in camera coordinates
+        self.rays_directions_cam = get_pix2cam(
+            self.image_resolution[0],
+            self.image_resolution[1],
+            self.camera.fx,
+            self.camera.fy,
         )
-
         # create world2cam matricies for images
-        bottom = torch.tensor([0, 0, 0, 1], dtype=torch.float32)
-        for img in self.scene_manager.images:
+        bottom = torch.tensor([0, 0, 0, 1]).reshape(1, 4)
+        for _, img in self.scene_manager.images.items():
             translation = torch.from_numpy(img.C()).reshape(3, 1)
             rotation = torch.from_numpy(img.R())
             world2cam = torch.concatenate(
                 [torch.concatenate([rotation, translation], dim=1), bottom], dim=0
-            )
+            ).float()
             self.world2cams.append(world2cam)
             # get image rgbs
             rgb = Image.open(self.scene_manager.image_path + img.name)
-            if (
-                rgb.width != self.image_resolution[0]
-                or rgb.height != self.image_resolution[1]
-            ):
+            if [rgb.width, rgb.height] != self.image_resolution:
                 rgb = rgb.resize(self.image_resolution, Image.Resampling.LANCZOS)
             rgb = self.transforms(rgb)
             self.rgbs.append(rgb)
-            ray_origins, ray_directions = get_rays(self.pixel_coords, world2cam)
+            ray_origins, ray_directions = get_rays(self.rays_directions_cam, world2cam)
             self.rays_origins.append(ray_origins)
             self.rays_directions.append(ray_directions)
 
