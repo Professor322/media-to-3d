adding image undistortion from google, plus fixing incorrect dimension in pixel coordinates

src/utils.py
@@ -32,12 +32,13 @@ def get_pix2cam(focal_x, focal_y, principal_point_x, principal_point_y):
     )
 
 
-def get_rays(H, W, pix2cam, pose):
+def get_rays(H, W, pix2cam, pose, distortion_params):
     """
     H - height
     W - width
     pix2cam - translation matrix from pixels to camera coordinates (3, 3)
     pose - tranlsation matrix from camera coordinates to world coordinates (3, 4)
+    distortion_params
     """
     x, y = get_pixel_coordinates(H, W)
 
@@ -46,6 +47,11 @@ def get_rays(H, W, pix2cam, pose):
 
     mat_vec_mul = lambda A, b: torch.matmul(A, b[..., None])[..., 0]
     camera_directions = mat_vec_mul(pix2cam, pixel_to_direction(x, y))
+    x, y = _radial_and_tangential_undistort(
+        camera_directions[..., 0], camera_directions[..., 1], **distortion_params
+    )
+
+    camera_directions = pixel_to_direction(x, y)
     # Switch from COLMAP (right, down, fwd) to OpenGL (right, up, back) frame.
     camera_directions = torch.matmul(
         camera_directions, torch.diag(torch.tensor([1.0, -1.0, -1.0]))
@@ -148,3 +154,84 @@ def pad_poses(p):
 def unpad_poses(p):
     """Remove the homogeneous bottom row from [..., 4, 4] pose matrices."""
     return p[..., :3, :4]
+
+
+def _compute_residual_and_jacobian(x, y, xd, yd, k1, k2, k3, k4, p1, p2):
+    """Auxiliary function of radial_and_tangential_undistort()."""
+    # Adapted from https://github.com/google/nerfies/blob/main/nerfies/camera.py
+    # let r(x, y) = x^2 + y^2;
+    #     d(x, y) = 1 + k1 * r(x, y) + k2 * r(x, y) ^2 + k3 * r(x, y)^3 +
+    #                   k4 * r(x, y)^4;
+    r = x * x + y * y
+    d = 1.0 + r * (k1 + r * (k2 + r * (k3 + r * k4)))
+
+    # The perfect projection is:
+    # xd = x * d(x, y) + 2 * p1 * x * y + p2 * (r(x, y) + 2 * x^2);
+    # yd = y * d(x, y) + 2 * p2 * x * y + p1 * (r(x, y) + 2 * y^2);
+    #
+    # Let's define
+    #
+    # fx(x, y) = x * d(x, y) + 2 * p1 * x * y + p2 * (r(x, y) + 2 * x^2) - xd;
+    # fy(x, y) = y * d(x, y) + 2 * p2 * x * y + p1 * (r(x, y) + 2 * y^2) - yd;
+    #
+    # We are looking for a solution that satisfies
+    # fx(x, y) = fy(x, y) = 0;
+    fx = d * x + 2 * p1 * x * y + p2 * (r + 2 * x * x) - xd
+    fy = d * y + 2 * p2 * x * y + p1 * (r + 2 * y * y) - yd
+
+    # Compute derivative of d over [x, y]
+    d_r = k1 + r * (2.0 * k2 + r * (3.0 * k3 + r * 4.0 * k4))
+    d_x = 2.0 * x * d_r
+    d_y = 2.0 * y * d_r
+
+    # Compute derivative of fx over x and y.
+    fx_x = d + d_x * x + 2.0 * p1 * y + 6.0 * p2 * x
+    fx_y = d_y * x + 2.0 * p1 * x + 2.0 * p2 * y
+
+    # Compute derivative of fy over x and y.
+    fy_x = d_x * y + 2.0 * p2 * y + 2.0 * p1 * x
+    fy_y = d + d_y * y + 2.0 * p2 * x + 6.0 * p1 * y
+
+    return fx, fy, fx_x, fx_y, fy_x, fy_y
+
+
+def _radial_and_tangential_undistort(
+    xd,
+    yd,
+    k1: float = 0,
+    k2: float = 0,
+    k3: float = 0,
+    k4: float = 0,
+    p1: float = 0,
+    p2: float = 0,
+    eps: float = 1e-9,
+    max_iterations=10,
+):
+    """Computes undistorted (x, y) from (xd, yd)."""
+    # From https://github.com/google/nerfies/blob/main/nerfies/camera.py
+    # Initialize from the distorted point.
+    x = torch.clone(xd)
+    y = torch.clone(yd)
+
+    for _ in range(max_iterations):
+        fx, fy, fx_x, fx_y, fy_x, fy_y = _compute_residual_and_jacobian(
+            x=x, y=y, xd=xd, yd=yd, k1=k1, k2=k2, k3=k3, k4=k4, p1=p1, p2=p2
+        )
+        denominator = fy_x * fx_y - fx_x * fy_y
+        x_numerator = fx * fy_y - fy * fx_y
+        y_numerator = fy * fx_x - fx * fy_x
+        step_x = torch.where(
+            torch.abs(denominator) > eps,
+            x_numerator / denominator,
+            torch.zeros_like(denominator),
+        )
+        step_y = torch.where(
+            torch.abs(denominator) > eps,
+            y_numerator / denominator,
+            torch.zeros_like(denominator),
+        )
+
+        x = x + step_x
+        y = y + step_y
+
+    return x, y